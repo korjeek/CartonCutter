a6a1f807f0621e592eb166ac11ad34ae6f588498
korjeek <olegka823@gmail.com>
2025-03-29 07:34:45 +0500

    create main-window

--- CartonCutter/Views/MainWindow.axaml.cs
+++ CartonCutter/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace CartonCutter.Views;
 
@@ -8,4 +12,30 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+    
+    private void OnCloseButtonClick(object sender, RoutedEventArgs e) => Close();
+    
+    private void OnChangeWindowStateButton(object sender, RoutedEventArgs e)
+    {
+        var image = ((Button)sender).Content as Image ?? 
+                    throw new ArgumentException("Sender is not Button or content is not Image");
+
+        if (WindowState == WindowState.Maximized)
+        {
+            WindowState = WindowState.Normal;
+            image.Source = new Bitmap(AssetLoader.Open(new Uri("avares://CartonCutter/Assets/maximize-512.png")));
+        }
+        else
+        {
+            // var screen = Screens.ScreenFromWindow(this)!;
+            // var workingArea = screen.WorkingArea;
+            // Position = workingArea.Position;
+            // Width = workingArea.Width;
+            // Height = workingArea.Height;
+            WindowState = WindowState.Maximized;
+            image.Source =new Bitmap(AssetLoader.Open(new Uri("avares://CartonCutter/Assets/restore-down-512.png")));
+        }
+    }
+    
+    private void OnMinimizeButtonClick(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
 }
