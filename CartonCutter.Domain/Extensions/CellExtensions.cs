e2ed4220212be7d401b17c4342ff84a2d9dac20c
artemchick35 <artemryzhkov2004@gmail.com>
2025-04-17 02:54:16 +0500

    fix ExcelParser

--- CartonCutter.Domain/Extensions/CellExtensions.cs
+++
@@ -1,9 +0,0 @@
-using NPOI.SS.UserModel;
-
-namespace CartonCutter.Domain.Extensions;
-
-public static class CellExtensions
-{
-    public static ICell SetCellValue(this ICell cell, int? value)
-        => value is null ? cell.SetBlank() : cell.SetCellValue(int.Parse(value.ToString()!));
-}
